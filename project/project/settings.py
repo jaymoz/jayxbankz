ce318e6b62d334bbc748257de6ec72aa5aaa3067
JACOB IDOKO <62575348+jaymoz@users.noreply.github.com>
2023-03-10 19:04:17 +0300

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -179,12 +179,12 @@ if os.getcwd() == '/app':
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jamosidoko@gmail.com'
-EMAIL_HOST_PASSWORD = 'josephidoko'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 
 
 
